32eafb25d3817670ad490987fad6a6a70c6d050c
Rusty Myers <rustymyers@gmail.com>
2011-11-15 09:17:22 -0500

    Update removeDeprecatedProducts.sh

--- removeDeprecatedProducts.sh
+++ removeDeprecatedProducts.sh
@@ -4,16 +4,35 @@
 
 # Place this script in the same folder as repoutil, otherwise it will not work properly.
 
+while getopts b:i: opt; do
+        case "$opt" in
+		b) branch="$OPTARG";;
+                i) items="$OPTARG";;
+		h) 
+			help
+			exit 0;;
+		\?)
+			help
+			exit 0;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
 # Sanity Check.  Makes sure a branch is specified.
-if [ -z $1 ]
-then
-        echo "Please specify a branch"
-        exit 1
+#if [ -z $1 ]
+#then
+#        echo "Please specify a branch"
+#        exit 1
+#fi
+
+# Set items to only remove Deprecated if nothing is specified
+if [ -z $items ]; then
+        items = "Deprecated"
 fi
 
 # Iterates through the output that shows deprecated items in a given branch.
 i=0
-for item in $(./repoutil --list-branch=${1} | grep Deprecated | cut -f1 -d ' ')
+for item in $(./repoutil --list-branch="$branch" | grep "$items" | cut -f1 -d ' ')
 do
         deprecatedProducts[$i]=$item
         let i++
